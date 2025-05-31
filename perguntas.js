ce19c1ed93966920e10cbf235587b16f4b866b99
mvitoria369 <fernandes.silva.maria2508@escola.pr.gov.br>
2025-05-30 21:32:33 -0300

    ........

---
+++ perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+  'Massas Italianas',
+   'Quais os ingredientes principais do macarrão?',
+   'Farinha de trigo e ovos')
+   
+criaCartao(
+  'Massas Italianas',
+  'Qual é a principal diferença entre massas frescas e massas secas?',
+  'A massa fresca é feita com ovos e deve ser consumida em pouco tempo. A massa seca é feita com sêmola de trigo e dura mais tempo.')
+   
+ criaCartao(
+  'Massas Italianas',
+  'O que significa “al dente” ao cozinhar massas?',
+  'Que a massa está cozida, mas ainda firme ao morder.')
+   
+  criaCartao(
+  'Massas Italianas',
+  'De onde vem o prato “spaghetti alla carbonara”?',
+  'Da região de Lazio, na Itália, especialmente de Roma.')
+   
+  criaCartao(
+  'Massas Italianas',
+  'Qual tipo de massa é recheada e pode conter queijo, carne ou vegetais?',
+  'Ravioli.')
+   
+  criaCartao(
+  'Massas Italianasl',
+  'Qual é a diferença entre tagliatelle e fettucine?',
+  'Ambas são fitas longas e planas, mas o tagliatelle é um pouco mais largo que o fettuccine.')
+   
+    criaCartao(
+    'Massas Italianas',
+    'Que molho tradicional é servido com penne all’arrabbiata?',
+    'Molho de tomate apimentado com alho e pimenta vermelha')
+   
+    criaCartao(
+    'Massas Italianas',
+    'Qual massa tem formato de pequenas conchas e segura bem molhos cremosos?',
+    'Conchiglie.')
+   
+    criaCartao(
+    'Massas Italianas',
+    'Qual é o nome da sopa italiana que leva massas pequenas, legumes e, às vezes, feijão?',
+    'Minestrone.')
+   
+    criaCartao(
+    'Massas Italianas',
+    'Qual é a massa longa e fina mais conhecida no mundo?',
+    'Espaguete (Spaghetti).')
+   
+    criaCartao(
+    'Massas Italianas',
+    'Por que algumas massas italianas tradicionais utilizam sêmola de trigo duro em vez de farinha comum?',
+    'Porque a sêmola de trigo duro tem mais proteína (glúten), o que dá mais elasticidade à massa e ajuda a manter a textura "al dente").')
+   
+    criaCartao(
+    'Massas Italianas',
+    'O que diferencia os molhos tradicionais do norte e do sul da Itália no uso com massas?',
+    'No norte, os molhos são mais cremosos e à base de manteiga, natas e queijos (como o molho Alfredo), enquanto no sul predominam os molhos com tomate, azeite de oliva, alho e pimenta (como o arrabbiata ou puttanesca).')
+   
